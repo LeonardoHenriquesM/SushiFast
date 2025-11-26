4ed8d75f5f1ff6aeecc151ecbf4f41e3717fb6e7
Thylia <thylia.brouillard@gmail.com>
2025-11-26 12:13:33 +0100

    main

--- SushiFast_final/src/main.jsx
+++ SushiFast_final/src/main.jsx
@@ -5,7 +5,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
