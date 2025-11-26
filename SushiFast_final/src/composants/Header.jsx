db12c3c4d5c72d4bd101fa205d9f2305cfedfa9a
leo.hm5 <leonardodacruzmateus@gmail.com>
2025-11-26 12:45:38 +0100

    début navbar

--- SushiFast_final/src/composants/Header.jsx
+++ SushiFast_final/src/composants/Header.jsx
@@ -2,35 +2,28 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">SushiFast</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Accueil</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/menus">Menus</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/filtres">Filtres</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light container-fluid">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <Link className="navbar-brand" to="/">SushiFast</Link>
+    <ul className="navbar-nav mr-auto mt-4 mt-lg-0">
+      <li>
+        <Link className="nav-item" to="/">Accueil</Link>
+      </li>
+      <li>
+        <Link className="nav-item" to="/menus">Menus</Link>
+      </li>
+      <li >
+        <Link className="nav-item" to="/filtres">Filtres</Link>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
   )
 }
